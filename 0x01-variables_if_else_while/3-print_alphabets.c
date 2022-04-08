5cf7354633c7dbd7ca8660bb997de6fda8b6f817
Jedid-Ahmed <44965714+Jedid-Ahmed@users.noreply.github.com>
2022-04-09 00:56:03 +0300

    d

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,19 +6,16 @@
  *
  * Return: Always 0 (Success)
 */
-
 int main(void)
 {
 	char ch = 'a';
 	char CH = 'A';
-
 	/*prints a - z*/
 	while (ch <= 'z')
 	{
 		putchar(ch);
 		++ch;
 	}
-
 	/*print A - Z*/
 	while (CH <= 'Z')
 	{
@@ -27,6 +23,5 @@ int main(void)
 		++CH;
 	}
 	putchar('\n');
-
 	return (0);
 }
