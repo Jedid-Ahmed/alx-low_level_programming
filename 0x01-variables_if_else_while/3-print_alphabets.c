bae0501c187715a6e17b54fb49dd6c041f857287
Jedid-Ahmed <44965714+Jedid-Ahmed@users.noreply.github.com>
2022-04-09 01:31:50 +0300

    d

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,11 +11,11 @@ int main(void)
 int c;
 
 for (c = 0; c < 26; ++c)
-putchar (a + c);
+putchar ('a' + c);
 for (c = 0; c < 26; ++c)
-putchar (A + c);
+putchar ('A' + c);
 
-putchar (n);
+putchar ('\n');
 
 return (0);
 }
