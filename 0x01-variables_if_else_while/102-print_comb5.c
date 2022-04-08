9b90a8851b626029aacda10286d6fd5da3eb6e89
Jedid-Ahmed <44965714+Jedid-Ahmed@users.noreply.github.com>
2022-04-09 02:03:59 +0300

    d

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,32 +12,32 @@ int ones;
 int t;
 int o;
 
-for (tens = 0; tens <= 9; tens++) /*print first two digit combo*/
+for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
 {
-for (ones = 0; ones <= 9; ones++)
+for (ones = '0'; ones <= '9'; ones++)
 {
- for (t = tens; t <= 9; t++) /*print second of pair*/
+for (t = tens; t <= '9'; t++) /*print second of pair*/
 {
-for (o = ones + 1; o <= 9; o++)
+for (o = ones + 1; o <= '9'; o++)
 {
 putchar(tens);
 putchar(ones);
-putchar( );
+putchar(' ');
 putchar(t);
 putchar(o);
 
-if (!((tens == 9 && ones == 8) &&
-      (t == 9 && o == 9)))
+if (!((tens == '9' && ones == '8') &&
+(t == '9' && o == '9')))
 {
-putchar(,);
-putchar( );
+putchar(',');
+putchar(' ');
 }
 }
-o = 0;
+o = '0';
 }
 }
 }
-putchar(n);
+putchar('\n');
 
 return (0);
-
+}
