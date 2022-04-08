452d344401af52edeeb9ad15da31d0060ac10655
Jedid-Ahmed <44965714+Jedid-Ahmed@users.noreply.github.com>
2022-04-09 01:52:32 +0300

    d

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -25,15 +25,15 @@ putchar(digit2 + 48);
 
 if (digit1 + digit2 != 17)
 {
-putchar(,);
-putchar( );
+putchar(',');
+putchar(' ');
 }
 }
 ++digit2;
 }
 ++digit1;
 }
-putchar(n);
+putchar('\n');
 
 return (0);
 }
