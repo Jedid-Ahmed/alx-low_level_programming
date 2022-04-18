511baacfba35b5e73aa2336134b1451f60fa6980
Jedid-Ahmed <jedidahmed88@gmail.com>
2022-04-19 00:39:07 +0300

    d

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,18 +1,32 @@
-#include "holberton.h"
-#include <stdio.h>
-
 /**
- * main - check the code for ALX School students.
+ * rev_string - reverse a string
  *
- * Return: Always 0.
+ * @s: string input pointer
+ *
+ * Return: nothing
 */
 
-int main(void)
+void rev_string(char *s)
 {
-	char s[10] = "Holberton";
+	int c = 0, f, l;
+	char temp;
 
-	printf("%s\n", s);
-	rev_string(s);
-	printf("%s\n", s);
-	return (0);
+	/*find length of string*/
+	while (c >= 0)
+	{
+		if (s[c] == '\0')
+			break;
+		c++;
+	}
+
+	/*reverse the string by swapping*/
+	for (f = 0; f < (c - 1); f++)
+	{
+		for (l = f + 1; l > 0; l--)
+		{
+			temp = *(s + l);
+			*(s + l) = *(s + (l - 1));
+			*(s + (l - 1)) = temp;
+		}
+	}
 }
