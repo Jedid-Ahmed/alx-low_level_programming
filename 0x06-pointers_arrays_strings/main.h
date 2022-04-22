99751bb032a83243038bdd62342055c6e9609774
Jedid-Ahmed <44965714+Jedid-Ahmed@users.noreply.github.com>
2022-04-22 15:10:05 +0300

    d

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,7 +8,10 @@
  */
 void reverse_array(int *a, int n);
 void print_number(int n);
-int _putchar(char c);
+int _putchar(char c)
+{
+putchar(c);
+}
 void print_buffer(char *b, int size);
 int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
