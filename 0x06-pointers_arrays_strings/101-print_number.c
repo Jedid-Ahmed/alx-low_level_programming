59ee79a7d9c56889c2b2ceee43848b8eee6d8c0e
Jedid-Ahmed <44965714+Jedid-Ahmed@users.noreply.github.com>
2022-04-22 14:13:10 +0300

    d

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,9 +1,16 @@
 #include "main.h"
+#include "stdio.h"
 /**
 * print_number - prints an integer
 *@n:integer to be printed
 *
 */
+void _putchar(char c)
+{
+
+putchar(c)
+
+}
 void print_number(int n)
 {
 
