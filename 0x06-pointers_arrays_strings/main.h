88f320ae95f711f80446148b00c239ec932e01d9
Jedid-Ahmed <44965714+Jedid-Ahmed@users.noreply.github.com>
2022-04-22 15:05:27 +0300

    d

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,7 +11,6 @@ void print_number(int n);
 void _putchar(char c);
 void print_buffer(char *b, int size);
 int _strcmp(char *s1, char *s2);
-int _putchar(char c)
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
