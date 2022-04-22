f67d86fc0e6c416fcd3d20e6ce96dbbe19146650
Jedid-Ahmed <jedidahmed88@gmail.com>
2022-04-22 13:47:57 +0300

    d

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,49 +1,13 @@
 #include "main.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* print_number - prints an integer
+*@n:integer to be printed
+*
+*/
 void print_number(int n)
 {
-	unsigned int tens, digit, positive = n;
-	double t_beg = 1;
 
-	if (n == 0)
-		_putchar('0');
-	else
-	{
-		if (n < 0)
-		{
-			positive = n * -1;
-			_putchar('-');
-		}
+unsigned int num = n;
+_putchar(num)
 
-		while (t_beg <= positive)
-			t_beg *= 10;
-		tens = t_beg / 10;
-
-		while (tens >= 1)
-		{
-			digit = positive / tens;
-			_putchar(digit + '0');
-			positive = (positive - (tens * digit));
-			tens /= 10;
-		}
-	}
-}
-int main(void)
-{
-    print_number(98);
-    _putchar('\n');
-    print_number(402);
-    _putchar('\n');
-    print_number(1024);
-    _putchar('\n');
-    print_number(0);
-    _putchar('\n');
-    print_number(-98);
-    _putchar('\n');
-    return (0);
 }
