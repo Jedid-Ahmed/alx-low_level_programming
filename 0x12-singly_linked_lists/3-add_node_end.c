cd23400310a7198c200e948f4f9b4fee8c1de57b
Jedid-Ahmed <jedidahmed88@gmail.com>
2022-05-19 12:51:20 +0300

    d

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,68 +1,50 @@
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
-#include "strlen.c"
-
-list_t *createNewNode(const char *str);
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
- * add_node_end - dds a new node at the end of a list_t list
- * @head: douple pointer to the head of the linked list
- * @str: pointer to string to be assigned to the added node's str property
- * Return: pointer to the new node (SUCCESS) OR
- * NULL, if there is insufficent memory available (FAILURE)
+ * add_node_end - is a function that adds a new node at the end of a list
+ * @str: is the string input
+ * @head: is the pointer to the start of the list
+ * Return: the address of the new element, or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *current_node = *head;
+	list_t *new;
+	list_t *temp;
+	unsigned int size = 0;
+	char *str2;
 
-	if (current_node)
+	str2 = strdup(str);
+
+	if (str2 == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = str2;
+	while (*str2)
 	{
-		while (current_node)
-		{
-			if (current_node->next)
-			{
-				current_node = current_node->next;
-			}
-			else
-			{
-				current_node->next = createNewNode(str);
-				return (current_node->next);
-			}
-		}
+		size++;
+		str2++;
+	}
+	new->len = size;
+	new->next = 0;
+
+	if (*head == 0)
+	{
+		*head = new;
 	}
 	else
 	{
-		*head = createNewNode(str);
+		temp = *head;
+		while (temp->next != 0)
+		{
+			temp = temp->next;
+		}
+		temp->next = new;
 	}
 
-	return (*head);
-}
-
-/**
- * createNewNode - create a new list_t list node
- * @str: pointer to string to be assigned to the created node's str property
- * Return: pointer to the new node (SUCCESS) OR
- * NULL, if there is insufficent memory available (FAILURE)
- */
-
-list_t *createNewNode(const char *str)
-{
-	list_t *new_node_ptr = malloc(sizeof(list_t));
-
-	if (!new_node_ptr)
-		return (NULL);
-
-	new_node_ptr->str = strdup(str);
-
-	if (!(new_node_ptr->str))
-	{
-		free(new_node_ptr);
-		return (NULL);
-	}
-
-	new_node_ptr->len = _strLen(new_node_ptr->str);
-	new_node_ptr->next = NULL;
-
-	return (new_node_ptr);
+	return (new);
 }
