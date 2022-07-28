c816a10d263fe8174dd279ed12bbc646800a39fa
Jedid-Ahmed <jedidahmed88@gmail.com>
2022-07-28 21:20:12 +0300

    Commits

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,15 +9,15 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_s *ht;
+	hash_table_t *ht;
 	unsigned long int i;
 
-	ht = malloc(sizeof(hash_table_s));
+	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
 
 	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_s *) * size);
+	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
